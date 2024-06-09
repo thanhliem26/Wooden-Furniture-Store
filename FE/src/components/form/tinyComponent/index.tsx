097522c2a55aacc0284105027e9d0d1ddf0e52c7
thanhliem <phamvanliem26122002@gmail.com>
2024-06-09 15:44:16 +0700

    'change-api-key'

--- FE/src/components/form/tinyComponent/index.tsx
+++ FE/src/components/form/tinyComponent/index.tsx
@@ -28,7 +28,7 @@ export default function EditorBox({
   return (
     <div className={styled['text_editor']}>
       <Editor
-        apiKey={import.meta.env.TINY_API_KEY}
+        apiKey={'kn1rpok46ehs4talvhuhbz9mn9sm0rjn3lom5dr5sp3k8l80'}
         value={value}
         //@ts-ignore
         onInit={(_evt, editor) => (editorRef.current = editor)}
